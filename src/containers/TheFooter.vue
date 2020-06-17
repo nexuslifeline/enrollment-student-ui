8fd416dfb48ac49d5408c5971193587749a4d404
mgatus <mrvgatus@gmail.com>
2020-06-17 22:44:51 +0800

    added the version on the footer

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -3,11 +3,20 @@
     <div>
       <span class="ml-1">&copy; {{new Date().getFullYear()}} <a href="http://stcedu.net/" target="_blank">Saint Theresa College of Tandag Inc.</a></span>
     </div>
+    <div class="ml-auto">
+      <span class="mr-1">version: </span>
+      {{version}}
+    </div>
   </CFooter>
 </template>
 
 <script>
 export default {
-  name: 'TheFooter'
+  name: 'TheFooter',
+  data() {
+    return {
+      version: process.env.VUE_APP_API_VERSION
+    }
+  },
 }
 </script>
